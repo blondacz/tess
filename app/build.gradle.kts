cd197deb261b983a3770786bd2eac1caf79f0446
blondacz <tomas.klubal@gmail.com>
2025-11-22 17:53:39 +0000

    Changelog and fixed gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,27 +4,7 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":domain"))
-    implementation(project(":adapters:octopus"))
-    implementation(project(":adapters:audit"))
-    implementation(project(":adapters:relay"))
-    implementation(project(":adapters:cylinder-temperature"))
-    implementation(libs.pekko.http)
-    implementation(libs.pekko.http.backend)
-    implementation(libs.pekko.stream)
-    implementation(libs.pekko.actor)
-    implementation(libs.pekko.circe)
-    implementation(libs.bundles.circe)
-    implementation(libs.bundles.prometheus)
-
-
     implementation(libs.logback)
-    implementation(libs.sttp.core)
-
-    testImplementation(libs.pekko.testkit)
-
-    testImplementation(libs.pekko.stream.testkit)
-    testImplementation(libs.pekko.http.testkit)
 }
 
 tasks.register("printVersion") {
